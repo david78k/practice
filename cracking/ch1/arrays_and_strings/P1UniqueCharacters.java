01c87cb04c215f55cc0613c5f46856d00c133c84
david78k <david78k@1055T>
2012-10-14 10:05:26 -0400

    cracking the coding problem 1

---
+++ cracking/ch1/arrays_and_strings/P1UniqueCharacters.java
@@ -0,0 +1,15 @@
+package ch1.arrays_and_strings;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+ * 
+ * @author david78k
+ *
+ */
+public class P1UniqueCharacters {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/Derivative.java
+++ src/Derivative.java
@@ -1,9 +1,14 @@
-
+/**
+ * Cracking the Coding Interview (Video Preview)
+ * http://www.youtube.com/watch?v=2cf9xo1S134
+ * 
+ * Question: Write a function to take the derivative of a polynomial
+ * 
+ * @author david78k
+ *
+ */
 public class Derivative {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
 	}
