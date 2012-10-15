4219cfc2b2696c71b8a4e153eaaee7a432a42796
david78k <root@c11node6.(none)>
2012-10-15 15:23:47 -0400

    P2

--- cracking/ch1/arrays_and_strings/P2ReverseString.c
+++ cracking/ch1/arrays_and_strings/P2ReverseString.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
+void reverse(char *); 
+
 int main(int argc, char **argv) {
 	char *str = "abcd";
-	char *tmp = str;
+	reverse(str);
+	printf("reversed string = %s\n", str);
+}
+
+void reverse (char * str) {
+	if (str == NULL) return;
+
+	char *end = str;
 	
-	while(*tmp) {
-		printf("%c", *tmp);
-		tmp++;
+	while(*end) {
+		printf("%c", *end);
+		end++;
 	}		
 	printf("\n");
+	end --;
 
-	while(*(--tmp)) 
-		printf("%c", *tmp);
+	char c;
+	while(str < end) {
+		printf("%c", *end);
+		//printf("%c", *str);
+		c = *str;
+		//printf("%c", c);
+		*str++ = *end;
+		*end-- = c;
+
+		//end --;
+		//str ++;
+	}
 	printf("\n");
 }
