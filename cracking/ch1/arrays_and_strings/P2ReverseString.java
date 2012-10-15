261476a397b35db75c377b426733242f88028b7c
david78k <root@c11node6.(none)>
2012-10-15 09:24:14 -0400

    P2

--- cracking/ch1/arrays_and_strings/P2ReverseString.java
+++ cracking/ch1/arrays_and_strings/P2ReverseString.java
@@ -9,10 +9,21 @@ package ch1.arrays_and_strings;
 public class P2ReverseString {
 
 	public static void main(String[] args) {
-
+		System.out.println(test("abcd"));
 	}
 
-	public void test(String str) {
-		
+	public static String test(String str) {
+		System.out.println(str);
+		StringBuffer sb = new StringBuffer();
+		int len = str.length();
+		for(int i = 0; i < str.length(); i ++) {
+			System.out.println(str.charAt(len-i-1));
+			sb.append(str.charAt(len-i-1));
+		}
+		/*for(int i = str.length() - 1; i == 0; i --) {
+			System.out.println(i + str.charAt(i));
+			sb.append(str.charAt(i));
+		}*/
+		return sb.toString();
 	}
 }
