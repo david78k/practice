e8ee3bf0186e73e9ece50bb2c6e92375b53c4581
david78k <root@c11node28.(none)>
2013-10-23 09:32:38 -0400

    SingleNumber

---
+++ leetcode/SingleNumber-template.java
@@ -0,0 +1,21 @@
+/*
+Single Number AC Rate: 3449/7510 
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+public class SingleNumber {
+	public int singleNumber(int[] A) {
+		// Note: The Solution object is instantiated only once and is reused by each test case.
+		return 0;
+	}
+	
+	public static void main (String[] args) {
+		int[] numbers = {1,1,2,3,3,4,4};
+
+		SingleNumber sn = new SingleNumber();
+		System.out.println(sn.singleNumber(numbers));
+	}
+}
