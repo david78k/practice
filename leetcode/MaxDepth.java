08b5df763adc624c7629e6c0351453d2efec9319
david78k <root@c11node28.(none)>
2013-10-24 09:28:00 -0400

    SingleNumber

---
+++ leetcode/MaxDepth.java
@@ -0,0 +1,6 @@
+Maximum Depth of Binary Tree AC Rate: 2790/6144 My Submissions
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+
--- leetcode/SingleNumber.java
+++ leetcode/SingleNumber.java
@@ -5,15 +5,32 @@ Given an array of integers, every element appears twice except for one. Find tha
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
+import java.util.*;
 
 public class SingleNumber {
 	public int singleNumber(int[] A) {
 		// Note: The Solution object is instantiated only once and is reused by each test case.
-		return 0;
+		Set<Integer> set = new HashSet<Integer>();
+		int sum = 0;
+
+		for (int n: A) {
+			//System.out.println(n);
+		/*	if (set.contains(n))
+				set.remove(n);
+			else
+				set.add(n);			
+		*/
+		//	sum += 10^n;
+			sum ^= n;
+			System.out.println(n + ": " + sum);
+		}
+		//Iterator<Integer> iter = set.iterator();
+		return sum^0;
+		//return iter.next();
 	}
 	
 	public static void main (String[] args) {
-		int[] numbers = {1,1,2,3,3,4,4};
+		int[] numbers = {1,4,3,2,3,1,4};
 
 		SingleNumber sn = new SingleNumber();
 		System.out.println(sn.singleNumber(numbers));
