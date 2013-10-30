790bc85b58edc7defdc5b462d4cbd0f15730f0e7
david78k <root@c11node28.(none)>
2013-10-30 11:13:23 -0400

    Stock2

--- leetcode/Stock2.java
+++ leetcode/Stock2.java
@@ -7,14 +7,67 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 
 public class Stock2 {
 	public int maxProfit(int [] prices) {
-		for (int p: prices) System.out.println(p);
-		return 0;
+		if(prices == null) return 0;
+		int profit = 0;
+		for(int i = 1; i < prices.length; i ++) {
+			if(prices[i] > prices[i - 1])
+				profit += prices[i] - prices[i - 1];
+		}		
+		return profit;
+	}
+
+	public int maxProfit2(int [] prices) {
+		if(prices == null) return 0;
+
+		int profit = 0;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int len = prices.length;
+		int p;
+		int begin = 0, end = 0;
+
+		while (begin < len) {
+			max = Integer.MIN_VALUE;
+			min = Integer.MAX_VALUE;
+			p = 0;
+			// find max
+			for (int i = begin; i < len; i ++) {
+				p = prices[i];
+				System.out.println(p);
+				if (max < p) {
+					max = p;
+					end = i;
+				}
+			}
+			//System.out.println("end = " + end);
+			// find min before max
+			for (int i = begin; i < end; i ++) {
+				min = Math.min(min, prices[i]);
+				//System.out.println(min);
+			}
+			
+			System.out.println("(max, min) = (" + max + ", " + min + ")");
+			if (begin != end) 
+				profit += (max - min);
+			begin = end + 1;	
+			System.out.println();
+		}
+	
+		return profit;
 	}
 		
 	public static void main (String[] args) {
 		Stock2 s = new Stock2();
-		int[] prices = {100, 200, 150};
-		
+		int[] prices = {100, 200, 150}; // max profit = 100
+		//int[] prices = {200, 130, 100, 150, 210, 80, 90}; // max profit = 120 = 110 + 10
+		//int[] prices = {100};
+		//int[] prices = {120, 100, 180};
+
+		// check null prices
+		//prices = null;
+		// check empty prices
+		//int[] prices = {};
+
 		System.out.println(s.maxProfit(prices));	
 	}	
 }
