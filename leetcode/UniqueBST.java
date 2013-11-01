2ac54b9e839cfbf1e35452bef3c79b3ead02b23e
david78k <root@c11node28.(none)>
2013-11-01 13:07:05 -0400

    UniqueBST

--- leetcode/UniqueBST.java
+++ leetcode/UniqueBST.java
@@ -1,3 +1,4 @@
+/*
 Unique Binary Search Trees Total Accepted: 2233 Total Submissions: 6642 My Submissions
 Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 
@@ -9,10 +10,43 @@ Given n = 3, there are a total of 5 unique BST's.
      3     2     1      1   3      2
     /     /       \                 \
    2     1         2                 3
+*/
 
 public class UniqueBST {
     public int numTrees(int n) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+	if (n <= 1) return 1;
+	
+	int sum = 0;
+	int left = 0; 
+	int right = 0;
+
+	for (int i = 1; i <= n; i ++) {
+		left = numTrees(i - 1);
+		right = numTrees(n - i);
+		sum += left * right;
+	}
+		
+	return sum;
     }
+
+	public int numTreesCatalan(int n) {
+		int c = 1;
+	
+		for (int i = 2; i <= n; i ++) 
+			c = 2 * (2*i - 1)*c / (i + 1);
+	
+		return c;
+	}
+
+	public static void main(String[] args) {
+		UniqueBST bst = new UniqueBST();
+		int n = 3;
+		n = 0;
+	//	n = 1;
+	//	n = 5;
+		n = 4;
+		System.out.println(bst.numTrees(n));
+	}
 }
