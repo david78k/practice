8f3d2fafbd33fd3deb31888efc86ae695a088b62
david78k <root@c11node28.(none)>
2013-11-05 11:10:01 -0500

    SymmetricTree

---
+++ leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+/*
+Remove Duplicates from Sorted Array Total Accepted: 2889 Total Submissions: 8645 My Submissions
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+*/
+
+public class RemoveDuplicatesFromSortedArray {
+	public int removeDuplicates(int[] A) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		RemoveDuplicatesFromSortedArray array = new RemoveDuplicatesFromSortedArray();
+		int[] A = {};
+
+		System.out.println(array.removeDuplicates(A));
+	}
+}
--- leetcode/SymmetricTree.java
+++ leetcode/SymmetricTree.java
@@ -45,15 +45,109 @@ The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
  *  }
  */
 
+import java.util.*;
+
 public class SymmetricTree {
+	private boolean help(TreeNode left, TreeNode right) {
+		if(left == null && right == null) return true;
+		if(left == null || right == null) return false;
+		if(left.val != right.val) return false;
+		return help (left.left, right.right) && help (left.right, right.left);	
+	}
+
     public boolean isSymmetric(TreeNode root) {
+		if(root == null) return true;
+
+		return help(root.left, root.right);
+	}
+
+    public boolean isSymmetricOneQueue(TreeNode root) {
+		if(root == null) return true;
+
+		TreeNode l, r;
+		LinkedList<TreeNode> q = new LinkedList<TreeNode>();
 		
-		return false;
+		q.addLast(root.left);
+		q.addLast(root.right);
+	
+		while(q.size() > 0) {
+			l = q.removeFirst();
+			r = q.removeFirst();						
+			if(l == null && r == null) continue;
+			if(l == null || r == null || l.val != r.val) return false;
+			
+			System.out.println(l.val + " " + r.val);
+
+			q.addLast(l.left);
+			q.addLast(r.right);
+			q.addLast(l.right);
+			q.addLast(r.left);
+		}
+
+		System.out.println("done");
+
+		return true;
+	}
+
+    public boolean isSymmetricTwoQueues(TreeNode root) {
+		if(root == null) return true;	
+
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+		TreeNode ltop, rtop;
+
+		LinkedList<TreeNode> leftQ = new LinkedList<TreeNode>();
+		LinkedList<TreeNode> rightQ = new LinkedList<TreeNode>();
+
+		leftQ.addLast(left);
+		rightQ.addLast(right);
+
+		while(leftQ.size() > 0) {
+			//if(rightQ.size() == 0) return false;
+			ltop = leftQ.removeFirst();
+			rtop = rightQ.removeFirst();
+				
+			System.out.println(ltop + " " + rtop);
+
+			//if(ltop != rtop) return false;
+			if(ltop == null && rtop != null) return false;
+			if(ltop != null && rtop == null) return false;
+			if(ltop == null && rtop == null) continue;
+			if(ltop.val != rtop.val) return false;
+
+			if(ltop != null) {
+				leftQ.addLast(ltop.left);
+				leftQ.addLast(ltop.right);
+			}
+			if(rtop != null) {
+				rightQ.addLast(rtop.right);
+				rightQ.addLast(rtop.left);
+			}
+		}
+
+		System.out.println("done");
+
+		if(rightQ.size() > 0) return false;
+
+		return true;
 	}
 
 	public static void main(String[] args) {
 		SymmetricTree st = new SymmetricTree();
 		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(2);
+
+		root.left.left = new TreeNode(3);
+		root.left.right = new TreeNode(4);
+
+		root.right.left = new TreeNode(4);
+		root.right.right = new TreeNode(3);
+
+		//root = null;
+//		root.left = null;
+//		root.right = null;
+
 		System.out.println(st.isSymmetric(root));
 	}	
 }
