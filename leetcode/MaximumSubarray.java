bd91bb6ef3c0c6d9f1d2d7100b0ace5904992f36
david78k <root@c11node28.(none)>
2013-11-06 11:21:02 -0500

    RemoveDuplicatesFromSortedArray

---
+++ leetcode/MaximumSubarray.java
@@ -0,0 +1,28 @@
+/*
+Maximum Subarray Total Accepted: 2836 Total Submissions: 8605 My Submissions
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+click to show more practice.
+
+More practice:
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] A) {
+		int len = A.length;
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		MaximumSubarray array = new MaximumSubarray();
+		//int[] A = {};
+		int[] A = {-2,1,-3,4,-1,2,1,-5,4};
+		//A = null;
+
+		array.maxSubArray(A);	
+	}
+}
--- leetcode/RemoveDuplicatesFromSortedArray.java
+++ leetcode/RemoveDuplicatesFromSortedArray.java
@@ -12,13 +12,48 @@ Your function should return length = 2, and A is now [1,2].
 
 public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] A) {
-		return 0;
+		int len = A.length;
+
+		if(len < 2) return len;
+
+		int i = 0, end = 1;
+
+		System.out.print(A[i] + ", ");
+
+		while (end < len) {
+			System.out.print(A[end] + ", ");
+			
+			if(A[end] != A[i]) 
+				A[++i] = A[end];
+
+			end ++;
+		}
+		System.out.println(A[len - 1]);
+
+		return i + 1;
+	}
+
+	public static void print(int[] A) {
+		for(int a: A) System.out.print(a + ", ");
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
 		RemoveDuplicatesFromSortedArray array = new RemoveDuplicatesFromSortedArray();
-		int[] A = {};
+		//int[] A = {};
+		//int[] A = {1};
+		//int[] A = {1,2};
+		//int[] A = {1,1,2};
+		//int[] A = {1,1,1,2};
+		int[] A = {1,1,1,1};
+		//int[] A = {1,1,1,2,2,3,3};
+		//int[] A = {1,1,1,2,2,3,3,4};
+		//int[] A = {1,1,1,2,2,3,4,4,4};
+		//int[] A = {1,1,1,2,2,3,3,4,5};
+		//int[] A = {1,1,1,2,2,3,3,4,5,5};
 
-		System.out.println(array.removeDuplicates(A));
+		print(A);
+		System.out.println("new length = " + array.removeDuplicates(A));
+		print(A);
 	}
 }
